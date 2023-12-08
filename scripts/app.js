f8e4890ffd183de3708a6a3f57c65a13eae7088b
MonoToneMoe <gghaddadgg@gmail.com>
2023-12-08 07:38:27 -0800

    changes

--- scripts/app.js
+++ scripts/app.js
@@ -15,26 +15,38 @@ LocalWeather().then(({ localLat, localLong }) => {
   long = localLong;
 
   console.log(lat, long);
-  WeatherApiTest();
+  FiveDayForecast();
   HourlyForecast();
 });
 
-function WeatherApiTest() {
+function FiveDayForecast() {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${weatherKey}&units=imperial`)
     .then((response) => response.json())
     .then((result) => {
       const dailyTemps = {};
+
       result.list.forEach((item) => {
-        const date = item.dt_txt.split(" ")[0];
+        // Use the Date object to convert the date string to a day of the week
+        const date = new Date(item.dt_txt).toLocaleDateString('en-US', { weekday: 'short' });
         const temperature = item.main.temp;
+
         if (!dailyTemps[date] || temperature < dailyTemps[date].min) {
           dailyTemps[date] = { min: temperature, max: temperature };
         }
+
         if (temperature > dailyTemps[date].max) {
           dailyTemps[date].max = temperature;
         }
       });
-      console.log(dailyTemps);
+      Object.keys(dailyTemps).forEach((date, index) => {
+        const minMaxElement = document.getElementById(`mm${index + 1}`);
+        const dayElement = document.getElementById(`d${index + 1}`);
+
+        if (minMaxElement && dayElement) {
+          minMaxElement.textContent = `Min: ${Math.round(dailyTemps[date].min)}°F Max: ${Math.round(dailyTemps[date].max)}°F`;
+          dayElement.textContent = date;
+        }
+      });
     });
 }
 
@@ -42,10 +54,47 @@ function HourlyForecast() {
   fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m&hourly=temperature_2m&temperature_unit=fahrenheit&forecast_days=1`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(
-        "Hourly Forecast:",
-        data.hourly.temperature_2m,
-        data.current.temperature_2m
-      );
+      // Extract time and temperature data
+      const timeArray = data.hourly.time.map((timestamp) => {
+        // Convert timestamp to a Date object
+        const date = new Date(timestamp);
+
+        // Extract only the time part (HH:mm:ss)
+        const time = date.toLocaleTimeString('en-US', { hour12: false });
+
+        return time;
+      });
+      const temperatureArray = data.hourly.temperature_2m;
+
+      // Log the extracted data (optional)
+      console.log("Time Array:", timeArray);
+      console.log("Temperature Array:", temperatureArray);
+
+      // If you have a line chart library (e.g., Chart.js), you can call a function to update the chart
+      updateChart(timeArray, temperatureArray);
     });
+}
+
+function updateChart(timeArray, temperatureArray) {
+  const ctx = document.getElementById('myChart').getContext('2d');
+
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: timeArray,
+      datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: 'rgba(225, 225, 255, 1.0)',
+        borderColor: 'rgba(0, 0, 255, 1)',
+        data: temperatureArray
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      // Add other chart options as needed
+    }
+  });
 }
